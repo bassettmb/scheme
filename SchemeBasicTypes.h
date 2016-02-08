5cef25695718f2bbb2505ff63f1d2557d9429fcf
Michael Bassett <michael.b.bassett@gmail.com>
2016-02-08 11:23:48 -0600

    Add function for marshaling C booleans into Scheme.

--- SchemeBasicTypes.h
+++ SchemeBasicTypes.h
@@ -16,4 +16,16 @@ extern const SchemeCons *const SchemeOptionNone;
 extern const SchemeCons *const SchemeOptionSome;
 extern const SchemeType SchemeEither;
 
+static void
+SchemeMarshalBool(Scheme *context, bool value)
+{
+  SchemeObj *val = &context->value;
+  val->hdr.kind = SCHEME_KIND_VAL;
+  val->hdr.variant = value ?
+    SchemeBoolTrue->hdr.variant : SchemeBoolFalse->hdr.variant;
+  val->hdr.size = 0;
+  val->word.type = &SchemeBool;
+}
+
+
 #endif /* !SCHEME_BASIC_TYPES_H */
